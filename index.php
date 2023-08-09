8a2d05507bbd7e17176122f861ba0748f78c11e3
Zayaret <zayaretrr@outlook.com>
2023-08-09 14:23:11 -0600

    Acomode Comentarios
    Quite espacio de comentarios

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-$token = '6695402130:AAGhD9Ovmwk2ysmwgTJvvHwjmx-fyA1E8DA'; // Este fue el token generado por Botfather
-                                                            //para la creacion del bot
+$token = '6695402130:AAGhD9Ovmwk2ysmwgTJvvHwjmx-fyA1E8DA'; // Este fue el token generado por Botfather para la creacion del bot
 $website = 'https://api.telegram.org' .$token;
 ?>
