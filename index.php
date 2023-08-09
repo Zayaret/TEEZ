9bd7cee787637d10bf20b5414391b24ff830505c
BrianBarona <141084297+BrianBarona@users.noreply.github.com>
2023-08-09 14:20:29 -0600

    Agregue cometario
    describi la funcion del token

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-$token = '6695402130:AAGhD9Ovmwk2ysmwgTJvvHwjmx-fyA1E8DA';
+$token = '6695402130:AAGhD9Ovmwk2ysmwgTJvvHwjmx-fyA1E8DA'; // Este fue el token generado por Botfather
+                                                            //para la creacion del bot
 $website = 'https://api.telegram.org' .$token;
 ?>
