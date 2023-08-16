4c977419e431302d61b03f7df0f18468d74294bf
BrianBarona <141084297+BrianBarona@users.noreply.github.com>
2023-08-15 23:44:05 -0600

    Cierre y Apertura
    Se agreggaron las apertutras y cierres del codigo con sus comentarios

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php // Apertura de codigo PHP
 $token = '6695402130:AAGhD9Ovmwk2ysmwgTJvvHwjmx-fyA1E8DA'; // Este fue el token generado por Botfather para la creacion del bot
 $website = 'https://api.telegram.org' .$token;
-?>
+//Cierre de PhP?> 
